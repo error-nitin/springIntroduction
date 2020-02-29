cd90d7ad38177a96bddbfc2a5f25f3d320440449
error-nitin <error.nitin@gmail.com>
2020-02-29 17:50:07 +0530

    comment added

--- src/main/java/com/springintroduction/test/SpringIntroduction/config/SpringIntroductionConfig.java
+++ src/main/java/com/springintroduction/test/SpringIntroduction/config/SpringIntroductionConfig.java
@@ -3,6 +3,11 @@ package com.springintroduction.test.SpringIntroduction.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Spring configuration file for scanning all the component classes.
+ * @author error.nitin@gmail.com
+ *
+ */
 @Configuration
 @ComponentScan(basePackages = {"com.springintroduction.test.SpringIntroduction"})
 public class SpringIntroductionConfig {
--- src/main/java/com/springintroduction/test/SpringIntroduction/initializer/EmployeeInitializer.java
+++ src/main/java/com/springintroduction/test/SpringIntroduction/initializer/EmployeeInitializer.java
@@ -1,6 +1,8 @@
 package com.springintroduction.test.SpringIntroduction.initializer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.springintroduction.test.SpringIntroduction.pojo.Address;
@@ -8,10 +10,21 @@ import com.springintroduction.test.SpringIntroduction.pojo.Department;
 import com.springintroduction.test.SpringIntroduction.pojo.Employee;
 import com.springintroduction.test.SpringIntroduction.pojo.SalaryInformation;
 
+/**
+ * Class to initialize employee object.
+ * This class can be use to provide default employee behavior.
+ * @author error.nitin
+ *
+ */
 @Component(value="employeeInitialize")
+@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class EmployeeInitializer {
 	private Employee employee;
 	
+	/**
+	 * Method to get an {@link Employee} object.
+	 * @return {@link Employee}
+	 */
 	public Employee getEmployee() {
 		return this.employee;
 	}
@@ -21,6 +34,11 @@ public class EmployeeInitializer {
 		this.employee = employee;
 	}
 	
+	/**
+	 * Method to initailize employee object with name and id
+	 * @param name - employee name
+	 * @param id - employee id
+	 */
 	public void initEmployee(String name, String id) {
 		employee.setEmpName(name);
 		employee.setEmpId(id);
@@ -28,6 +46,11 @@ public class EmployeeInitializer {
 		this.initDepartment(employee.getDept());
 		this.initSalary(employee.getSalaryInfo());
 	}
+	
+	/**
+	 * Method to initialize employee address
+	 * @param address - {@link Address}
+	 */
 	public void initAddress(Address address) {
 		address.setAddress1("Somewhere on the planet");
 		address.setCity("Some city");
@@ -36,11 +59,19 @@ public class EmployeeInitializer {
 		address.setZipCode(1233456);		
 	}
 	
+	/**
+	 * Method to initialize {@link Department} of the employee
+	 * @param department {@link Department}
+	 */
 	public void initDepartment(Department department) {
 		department.setId(102);
 		department.setName("Engineering");
 	}
 	
+	/**
+	 * Method to initialize employee salary
+	 * @param salary {@link SalaryInformation}
+	 */
 	public  void initSalary(SalaryInformation salary) {
 		salary.setBasic(12000.000f);
 		salary.setDa(salary.getBasic()*0.45f);
